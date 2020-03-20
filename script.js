d523f3658dc3824439b9d712b5d0ea1fbd0b69c8
Maxsem <max.semegen@gmail.com>
2020-03-20 11:07:29 -0400

    define all elements

---
+++ script.js
@@ -0,0 +1,13 @@
+function  init() {
+
+    const currentTempEl = document.getElementById("temperature");
+    const currentUVEl = document.getElementById("UVIndex");
+    const currentWindEl = document.getElementById("windSpeed");
+    const currentHumidityEl = document.getElementById("humidity");
+    const inputEl = document.getElementById("cityInput");
+    const currentPicEl = document.getElementById("currentPicture");
+    const nameEl = document.getElementById("cityName");
+    const searchEl = document.getElementById("searchButton");
+    
+   
+         init();
